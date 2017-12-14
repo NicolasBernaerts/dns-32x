33f1712db7047fe23b8ae93f0ee93fd4d0cb561f
Nicolas Bernaerts <nicolas.bernaerts@gmail.com>
2017-12-14 14:58:07 +0100

    Update dashbutton-goodnight.sh

--- dashbutton/dashbutton-goodnight.sh
+++ dashbutton/dashbutton-goodnight.sh
@@ -1,15 +1,15 @@
 #!/ffp/bin/sh
 
-# PROVIDE: dashbutton-goodnight
+# PROVIDE: dashbutton-daemon
 # REQUIRE: LOGIN
 
 . /ffp/etc/ffp.subr
 
-name="dashbutton-goodnight"
+name="dashbutton-daemon"
 command="/ffp/sbin/dashbutton"
 start_cmd="dashbutton_start"
 
-dashbutton_flags="--mac-address AC:63:BE:76:41:36 --net-interface egiga0 --action telegram-goodnight"
+dashbutton_flags="--mac-address AC:63:BE:76:41:36 --net-interface egiga0 --action dashbutton-goodnight"
 
 # start dash button detection script as a background process
 dashbutton_start()
